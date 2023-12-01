e5ef5b54df6d04fcf456edf90a2a7154e84fa9df
mallick-portfolio <tamal1714019@gmail.com>
2023-12-01 16:49:02 +0600

    update ..

--- basic_template/wsgi.py
+++ basic_template/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for basci_template project.
+WSGI config for basic_template project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basci_template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basic_template.settings')
 
 application = get_wsgi_application()
